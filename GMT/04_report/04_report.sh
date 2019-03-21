141e84a2c8279e767a958fcc1eb2eac8fe2a6f52
107602512 <107602512@cc.ncu.edu.tw>
2019-03-21 19:55:30 +0800

    0321

--- GMT/04_report/04_report.sh
+++ GMT/04_report/04_report.sh
@@ -9,7 +9,7 @@ gmt set PS_MEDIA A4
 gmt psxy -R0/1/0/1 -JX1c -T -K -P > $out_ps
 gmt psbasemap -R119/123/21/26 -Jm3 -BwEsN -B1 -G0/250/250 -X-0.5 -Y5 -O -K >> $out_ps
 gmt pscoast -R -J -K -G250/250/0 -W1 -Df -Ia -O -K >> $out_ps
-awk '{print $1, $2, $3}' $input_list | gmt psxy -R -J -Sc0.05 -O -K >> $out_ps
+awk '{if ($4>=3 && $4<4) print $1, $2, $3}' $input_list | gmt psxy -R -J -Sc0.05 -C$cpt-O -K >> $out_ps
 echo 121.29 22.83 | gmt psxy -R -J -Sa0.7 -W0.01 -G255/0/0 -O -K >> $out_ps
 echo "121.29 22.7 M:4.6 Depth:5.9km" | gmt pstext -R -J -O -K -F+f,Helvetica-Bold,black >> $out_ps
 

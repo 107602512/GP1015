d1f2bf69479ce509caeed85c8fc8261a87982ebb
107602512 <107602512@cc.ncu.edu.tw>
2019-04-11 18:29:15 +0800

    0411

--- GMT/HW.sh
+++ GMT/HW.sh
@@ -14,6 +14,8 @@ gmt psxy -R0/1/0/1 -JX1c -T -K -P > $out_ps
 gmt psbasemap -R119/123/21/26 -Jm3 -BwEsN -B1 -G197/226/226 -X-0.5 -Y5 -O -K>> $out_ps
 gmt pscoast -R -J -B -W1 -G205/205/152 -Df -Ia -O -K >> $out_ps
 awk '{print $1, $2, $3}' $input_list | gmt psxy -R -J -Sc0.3 -C$cpt -O -K >> $out_ps
+echo "122 25.8 PM2.5 concentration 2019Jan" | gmt pstext -R -J -O -K -F+f,Helvetica-Bold,black >> $out_ps
+
 
 # Bottom map
 gmt psbasemap -R119/123/0/60 -Jx3/-0.1 -Bxa1f1 -Bya5f5 -X0 -Y-7 -O -K >> $out_ps
